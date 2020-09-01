37747af6fdce2db7b0a43e462538e9e4e1fe615c
Kory Becker <kbecker@primaryobjects.com>
2020-08-31 21:52:53 -0400

    Update aiManager.js

--- wumpus/js/managers/aiManager.js
+++ wumpus/js/managers/aiManager.js
@@ -150,9 +150,9 @@ const AiManager = {
         if (potentialRoom) {
           // Calculate Manhatten distance.
           const distance = Math.abs(potentialRoom.y - y) + Math.abs(potentialRoom.x - x);
-          if (potentialRoom.visited < closestSafeRoom.visited) {
+          if (potentialRoom.knowledge.visited < closestSafeRoom.visited) {
 //if (distance < closestSafeRoom.distance) {
-            closestSafeRoom = { visited/*distance*/, room: potentialRoom };
+            closestSafeRoom = { visited: potentialRoom.knowledge.visited/*distance*/, room: potentialRoom };
           }
         }
       }
